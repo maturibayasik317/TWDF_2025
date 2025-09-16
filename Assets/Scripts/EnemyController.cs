69fb18979069353bb25b9914efff0bdb4b9e17b7
aas <aaas>
2025-09-16 17:56:32 +0900

    アニメ制作完了、味方の攻撃を作る

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,10 +10,13 @@ public class EnemyController : MonoBehaviour
     [SerializeField, Header("移動速度")]
     private float speed;
     private Vector3[] path; // pathDataから取得した座標を格納するための配列
+    private Animator animator;
 
 
     void Start()
     {
+        TryGetComponent(out animator);
+
         // 経路を取得
         path = pathData.pathArray.Select(x => x.position).ToArray();
         // スタート地点に敵をセット
@@ -23,7 +26,9 @@ public class EnemyController : MonoBehaviour
         // 移動時間を計算 (距離 ÷ 速度)
         float moveDuration = totalDistance / speed;
         // 経路に沿って移動
-        transform.DOPath(path, moveDuration).SetEase(Ease.Linear);
+        transform.DOPath(path, moveDuration)
+                 .SetEase(Ease.Linear)
+                 .OnWaypointChange(x => ChangeWalkingAnimation(x));
         //敵がゴールについたとき
         transform.DOPath(path, 1000 / speed)
     .   SetEase(Ease.Linear)
@@ -50,5 +55,39 @@ public class EnemyController : MonoBehaviour
             length += Vector3.Distance(path[i], path[i + 1]);
         }
         return length;
+    }
+
+    // 敵の進行方向を取得してアニメを変更
+    private void ChangeWalkingAnimation(int index)
+    {
+        // 次の移動先がない場合は処理を終了
+        if (index >= path.Length)
+        {
+            return;
+        }
+        // 左方向
+        if (transform.position.x > path[index].x)
+        {
+            animator.SetFloat("Y", 0f);
+            animator.SetFloat("X", -1.0f);
+        }
+        // 上方向
+        else if (transform.position.y < path[index].y)
+        {
+            animator.SetFloat("X", 0f);
+            animator.SetFloat("Y", 1.0f);
+        }
+        // 下方向
+        else if (transform.position.y > path[index].y)
+        {
+            animator.SetFloat("X", 0f);
+            animator.SetFloat("Y", -1.0f);
+        }
+        // 右方向
+        else
+        {
+            animator.SetFloat("Y", 0f);
+            animator.SetFloat("X", 1.0f);
+        }
     }
 }
